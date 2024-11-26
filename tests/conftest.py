fddfed0560e63df5c3c7129d464842fbbc997854
Graham Hukill <ghukill@gmail.com>
2024-11-26 15:38:36 -0500

    Establish TIMDEXDataset class
    Why these changes are being introduced:
    
    All operations for a dataset can be wrapped in a TIMDEXDataset class.
    This class would be responsible for loading a dataset that read and
    write operations would be performed against.
    
    How this addresses that need:
    * Establishes TIMDEXDataset class
    * Adds methods for loading a database
    * Support for local or S3 location for dataset
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/TIMX-415

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,18 @@
 import pytest
 
+from timdex_dataset_api import TIMDEXDataset
+
 
 @pytest.fixture(autouse=True)
 def _test_env(monkeypatch):
     monkeypatch.setenv("TDA_LOG_LEVEL", "INFO")
+
+
+@pytest.fixture
+def local_dataset_location():
+    return "tests/fixtures/local_datasets/dataset"
+
+
+@pytest.fixture
+def local_dataset(local_dataset_location):
+    return TIMDEXDataset.load(local_dataset_location)
