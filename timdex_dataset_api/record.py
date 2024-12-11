0235289fab7d22aedadc52bed219b909c1ab14e2
jonavellecuerdo <cuerdojonavelle@gmail.com>
2024-12-11 15:00:10 -0500

    Refactor DatasetRecord to use attrs
    Why these changes are being introduced:
    * Reworking the dataset partitions to use the [year, month, day]
    of the 'run_date' means that parquet files for different 'source' runs
    on the same 'run_date' get written to the same partition directory.
    Therefore, it is crucial that the timdex_dataset_api.write method
    retrieves the correct partition columns from the (batches) of DatasetRecord
    objects. The DatasetRecord class has been refactored to adhere
    to the following criteria:
    
    1. When writing to the dataset, and therefore serializing DatasetRecord objects,
       year, month, day should be derived from the run_date and should not be modifiable
    2. If possible, avoid parsing a datetime string 3 times for each partition column
    
    How this addresses that need:
    * Refactor DatasetRecord to use attrs
    * Define custom strict_date_parse converter method for 'run_date' field
    * Simplify serialization method to rely on converter for 'run_date'
      error handling
    * Remove DatasetRecord.validate
    * Include attrs as a dependency
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/TIMX-432

--- timdex_dataset_api/record.py
+++ timdex_dataset_api/record.py
@@ -1,12 +1,15 @@
 """timdex_dataset_api/record.py"""
 
-import datetime
-from dataclasses import asdict, dataclass
+from datetime import UTC, datetime
 
-from timdex_dataset_api.exceptions import InvalidDatasetRecordError
+from attrs import asdict, define, field
 
 
-@dataclass
+def strict_date_parse(date_string: str) -> datetime:
+    return datetime.strptime(date_string, "%Y-%m-%d").astimezone(UTC)
+
+
+@define
 class DatasetRecord:
     """Container for single dataset record.
 
@@ -16,56 +19,34 @@ class DatasetRecord:
     """
 
     # primary columns
-    timdex_record_id: str
-    source_record: bytes
-    transformed_record: bytes
-    source: str
-    run_date: str | datetime.datetime
-    run_type: str
-    run_id: str
-    action: str
+    timdex_record_id: str = field()
+    source_record: bytes = field()
+    transformed_record: bytes = field()
+    source: str = field()
+    run_date: datetime = field(converter=strict_date_parse)
+    run_type: str = field()
+    run_id: str = field()
+    action: str = field()
 
-    # partition columns
-    year: str | None = None
-    month: str | None = None
-    day: str | None = None
+    @property
+    def year(self) -> str:
+        return self.run_date.strftime("%Y")
 
-    def __post_init__(self) -> None:
-        """Post init method to derive partition values from self.run_date"""
-        run_date = self.run_date
+    @property
+    def month(self) -> str:
+        return self.run_date.strftime("%m")
 
-        if isinstance(run_date, str):
-            try:
-                run_date = datetime.datetime.strptime(run_date, "%Y-%m-%d").astimezone(
-                    datetime.UTC
-                )
-            except ValueError as exception:
-                raise InvalidDatasetRecordError(
-                    "Cannot parse partition values [year, month, date] from invalid 'run-date' string."  # noqa: E501
-                ) from exception
-
-        self.year = run_date.strftime("%Y")
-        self.month = run_date.strftime("%m")
-        self.day = run_date.strftime("%d")
+    @property
+    def day(self) -> str:
+        return self.run_date.strftime("%d")
 
     def to_dict(
         self,
-        *,
-        validate: bool = True,
     ) -> dict:
         """Serialize instance as dictionary."""
-        if validate:
-            self.validate()
-
-        return asdict(self)
-
-    def validate(self) -> None:
-        """Validate DatasetRecord for writing."""
-        # ensure all partition columns are set
-        missing_partition_values = [
-            field for field in ["year", "month", "day"] if getattr(self, field) is None
-        ]
-        if missing_partition_values:
-            raise InvalidDatasetRecordError(
-                f"Partition values are missing: {', '.join(missing_partition_values)}"
-            )
+        return {
+            **asdict(self),
+            "year": self.year,
+            "month": self.month,
+            "day": self.day,
+        }
