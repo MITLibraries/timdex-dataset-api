6b93d88997b1a64c8fc2749afc64ff1ed0c9cb1a
jonavellecuerdo <cuerdojonavelle@gmail.com>
2024-12-13 15:23:56 -0500

    Replace static fixture of local dataset with temp, dynamic dataset
    Why these changes are being introduced:
    * This avoids the creation of artifacts in the 'tests/fixtures/' directory
    that become outdated if / when partitioning scheme changes. This also
    updates the 'local_dataset' fixture to use the 'tmp_path' fixture from
    pytest, allowing each test that uses the 'local_dataset' fixture
    to run the test in a clean and isolated environment.
    
    How this addresses that need:
    * Update "local_dataset" fixtures
    * Update unit tests
    
    Side effects of this change:
    * None
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/TIMX-440

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,10 @@
 
 import pytest
 
-from tests.utils import generate_sample_records
+from tests.utils import (
+    generate_sample_records,
+    generate_sample_records_with_simulated_partitions,
+)
 from timdex_dataset_api import TIMDEXDataset
 
 
@@ -19,18 +22,22 @@ def _test_env(monkeypatch):
 
 
 @pytest.fixture
-def local_dataset_location():
-    return "tests/fixtures/local_datasets/dataset"
+def local_dataset_location(tmp_path):
+    return str(tmp_path / "tests/fixtures/local_datasets/dataset")
 
 
 @pytest.fixture
 def local_dataset(local_dataset_location):
-    return TIMDEXDataset.load(local_dataset_location)
+    timdex_dataset = TIMDEXDataset(local_dataset_location)
+    records = generate_sample_records_with_simulated_partitions(num_records=5_000)
+    timdex_dataset.write(records)
+    timdex_dataset.load()
+    return timdex_dataset
 
 
 @pytest.fixture
-def new_dataset(tmp_path) -> TIMDEXDataset:
-    location = str(tmp_path / "new_dataset")
+def new_local_dataset(tmp_path) -> TIMDEXDataset:
+    location = str(tmp_path / "new_local_dataset")
     return TIMDEXDataset(location=location)
 
 
