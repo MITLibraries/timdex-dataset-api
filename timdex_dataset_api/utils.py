126ee8117a5209f10c216ceccc89a47829c415fd
Graham Hukill <ghukill@gmail.com>
2025-06-12 16:02:00 -0400

    Add utils docstrings

--- timdex_dataset_api/utils.py
+++ timdex_dataset_api/utils.py
@@ -9,6 +9,7 @@ def get_first_column_value_from_batch(
     batch: pa.RecordBatch,
     column: str,
 ) -> Any:  # noqa: ANN401
+    """Given a pyarrow RecordBatch, return the value of the first row from a column."""
     values = batch.column(column).to_pylist()
     if not values:
         raise ValueError(f"No values found for column '{column}' in batch.")
@@ -16,4 +17,5 @@ def get_first_column_value_from_batch(
 
 
 def get_batch_fragment_index(batch: pa.RecordBatch) -> int:
+    """Return the fragment index from a RecordBatch, by using the first row value."""
     return get_first_column_value_from_batch(batch, "__fragment_index")
