eac061079df38272cee59a1a869a60f3094eb7b1
Jonavelle Cuerdo <cuerdojonavelle@gmail.com>
2025-11-19 10:02:32 -0500

    Add DatasetEmbedding to root package imports (#177)

--- timdex_dataset_api/__init__.py
+++ timdex_dataset_api/__init__.py
@@ -1,13 +1,14 @@
 """timdex_dataset_api/__init__.py"""
 
 from timdex_dataset_api.dataset import TIMDEXDataset
-from timdex_dataset_api.embeddings import TIMDEXEmbeddings
+from timdex_dataset_api.embeddings import DatasetEmbedding, TIMDEXEmbeddings
 from timdex_dataset_api.metadata import TIMDEXDatasetMetadata
 from timdex_dataset_api.record import DatasetRecord
 
 __version__ = "3.6.1"
 
 __all__ = [
+    "DatasetEmbedding",
     "DatasetRecord",
     "TIMDEXDataset",
     "TIMDEXDatasetMetadata",
